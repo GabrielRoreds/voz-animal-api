2ae00d499bc78cc07d5a8765add18b7a3736c7a0
Jennefer-fig <212982475+Jennefer-fig@users.noreply.github.com>
2025-10-13 17:57:28 -0300

    adicionando a api

---
+++ api-zoonoses/index.js
@@ -0,0 +1,47 @@
+const express = require('express'); /*framework para criar servidores e rotas HTTP.*/
+const axios = require('axios'); /*biblioteca para fazer requisições HTTP (usada para consultar a BrasilAPI)*/
+const cors = require('cors'); /*permite que seu site (em outro domínio ou porta) acesse sua API sem bloqueio*/
+
+const app = express();
+const PORT = 3000; /*define a porta onde sua API vai rodar localmente (http://localhost:3000)*/
+
+app.use(cors()); // Libera acesso externo à API
+
+// Dados fictícios dos hospitais públicos veterinários
+const hospitais = [ /*dados*/
+  { nome: "Hospital Veterinário Zona Norte", bairro: "Vila Nova Cachoeirinha" },
+  { nome: "Hospital Veterinário Zona Sul", bairro: "Capão Redondo" },
+  { nome: "Hospital Veterinário Zona Leste", bairro: "Tatuapé" },
+  { nome: "Hospital Veterinário Zona Oeste", bairro: "Vila Sônia" }
+];
+
+// Rota para buscar dados com base no CEP
+app.get('/buscar', async (req, res) => {
+  const cep = req.query.cep;
+  if (!cep) return res.status(400).json({ erro: 'CEP não informado' });
+
+  try {
+    const response = await axios.get(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+    const { city, neighborhood, state } = response.data;
+
+    if (city !== 'São Paulo') {
+      return res.status(404).json({ erro: 'CEP fora de São Paulo' });
+    }
+    const hospitaisFiltrados = hospitais.filter(hospital => 
+    hospital.bairro.toLowerCase() === neighborhood.toLowerCase());
+
+    res.json({
+      cep,
+      cidade: city,
+      bairro: neighborhood,
+      estado: state,
+      hospitais_em_sp:hospitaisFiltrados
+    });
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao buscar CEP' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(` API atulizada rodando em http://localhost:${PORT}`);
+});
