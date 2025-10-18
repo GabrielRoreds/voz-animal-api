9f1edca4ea94482c64f6e08f808881642352842d
deceptionfalls <deceptionfalls@proton.me>
2025-10-18 16:14:17 -0300

    feat: definir endpoints

--- api-zoonoses/index.js
+++ api-zoonoses/index.js
@@ -1,11 +1,21 @@
 const express = require('express'); /*framework para criar servidores e rotas HTTP.*/
 const axios = require('axios'); /*biblioteca para fazer requisições HTTP (usada para consultar a BrasilAPI)*/
 const cors = require('cors'); /*permite que seu site (em outro domínio ou porta) acesse sua API sem bloqueio*/
+const path = require('path');
 
 const app = express();
 const PORT = 3000; /*define a porta onde sua API vai rodar localmente (http://localhost:3000)*/
 
-app.use(cors()); // Libera acesso externo à API
+// Definir endpoints
+app.use(express.static(path.join(__dirname, '../pages-public/')));
+app.use(express.static(path.join(__dirname, '../')));
+
+app.use(cors()); // Sem parâmetros aí todas as origens são aceitas
+
+// Definir endpoint padrão
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../pages-public/index.html'));
+});
 
 // Dados fictícios dos hospitais públicos veterinários
 const hospitais = [ /*dados*/
@@ -16,7 +26,7 @@ const hospitais = [ /*dados*/
 ];
 
 // Rota para buscar dados com base no CEP
-app.get('/buscar', async (req, res) => {
+app.get('/buscar', cors(), async (req, res) => {
   const cep = req.query.cep;
   if (!cep) return res.status(400).json({ erro: 'CEP não informado' });
 
@@ -27,15 +37,15 @@ app.get('/buscar', async (req, res) => {
     if (city !== 'São Paulo') {
       return res.status(404).json({ erro: 'CEP fora de São Paulo' });
     }
-    const hospitaisFiltrados = hospitais.filter(hospital => 
-    hospital.bairro.toLowerCase() === neighborhood.toLowerCase());
+    const hospitaisFiltrados = hospitais.filter(hospital =>
+      hospital.bairro.toLowerCase() === neighborhood.toLowerCase());
 
     res.json({
       cep,
       cidade: city,
       bairro: neighborhood,
       estado: state,
-      hospitais_em_sp:hospitaisFiltrados
+      hospitais_em_sp: hospitaisFiltrados
     });
   } catch (error) {
     res.status(500).json({ erro: 'Erro ao buscar CEP' });
